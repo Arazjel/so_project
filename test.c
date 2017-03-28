6926b8e9528a27cdda281598dc414bbcd1aefac4
Arazjel <karwowski.mar@gmail.com>
2017-03-28 08:59:28 +0200

    Add files via upload
    sprawdza czy obiekty w danym katalogu sa folderami czy nie

---
+++ test.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int main(int argc, char* argv[]){
+    	DIR * dir;
+    	struct dirent * dp;
+    	char * fileName;
+    	dir = opendir(argv[1]);
+	struct stat sb = {0};
+
+    	while ((dp=readdir(dir)) != NULL) {
+        	if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") )
+        	{
+           	 // do nothing (straight logic)
+        	} else {
+		fileName = dp->d_name;
+        	printf("%s\t",fileName);
+
+		if (stat(file_name, &sb) == 0 && S_ISDIR(sb.st_mode)){
+        		printf("Folder");
+    		}else {
+        		printf("NOT Folder");
+			/*
+				tu dać funkcjonalnosc jezeli
+				obiekt jest plikiem
+			*/
+		}
+		printf("\n");
+        }
+    }
+    closedir(dir);
+    return 0;
+
+}

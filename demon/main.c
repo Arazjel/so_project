fc54bd4dd4e36cd2d02362fbcfc2cc5b3c425a78
MnichWwy <mnichwwy@gmail.com>
2017-05-16 09:10:34 +0200

    Create main.c

---
+++ demon/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "sleepMode.h"
+#include "fileinfo.h"
+
+void isDirectory(char* argv[])
+{
+struct stat sb = {0};
+
+    if (!(stat(argv[1], &sb) == 0 && (S_ISDIR(sb.st_mode))))
+    {
+        fprintf(stderr, "Pierwszy z podanych katalogow nie istnieje.\n");
+	exit(EXIT_FAILURE);
+    }
+
+    if (!(stat(argv[2], &sb) == 0 && (S_ISDIR(sb.st_mode))))
+    {
+        fprintf(stderr, "Drugi z podanych katalogow nie istnieje.\n");
+	exit(EXIT_FAILURE);
+    }
+
+}
+
+int main(int argc,char** argv)
+{
+
+isDirectory(argv); //jesli podane foldery nie istnieja program wychodzi z komunikatem bledu
+
+
+
+return 0;
+}

3b3101c69fe1a202a0ef96b031da386f57038782
MnichWwy <mnichwwy@gmail.com>
2017-05-16 09:12:44 +0200

    Create fileinfo.h

---
+++ demon/fileinfo.h
@@ -0,0 +1,15 @@
+#ifndef FILEINFO_H
+#define FILEINFO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <err.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <time.h>
+
+void fileInfo (int argc_, char **argv_);
+
+
+#endif

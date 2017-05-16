5c72b927959ef6f2082dcd0ee86960df36124eea
MnichWwy <mnichwwy@gmail.com>
2017-05-16 09:15:55 +0200

    Create sleepMode.h

---
+++ demon/sleepMode.h
@@ -0,0 +1,13 @@
+#ifndef SLEEPMODE_H
+#define SLEEPMODE_H
+
+#define STANDARD_TIME 500
+
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <signal.h>
+
+void waitFor(unsigned int);
+
+#endif
